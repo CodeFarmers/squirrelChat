53882d17ab54445a881207cb3a422ffc68f6d3eb
Tom Lauwyck <tom.lauwyck@gmail.com>
2012-05-07 22:49:28 +0200

    hardcoded a testmessage and determined some places to do the file write and read

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,10 +2,14 @@ class ChatController < ApplicationController
   def new
     puts "In chatController NEW"
     @nickname = session[:tmp_nickname]
-    ##puts "The nickname in the chatController: " +  @nickname
+    ##TODO: This is where we could read older chat messages from file
+    @message = "This is a hard-coded test message"
   end
   
   def create
-    render '/chat/new'
+    @message = params[:message]
+    ##TODO: This is where we could write the message to a file
+    
+    redirect_to chat_path
   end
 end
