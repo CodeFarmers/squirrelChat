18120a857283a82c4ee64f4fce4467ca2d5c07f5
Tom Lauwyck <tom.lauwyck@gmail.com>
2012-05-07 18:32:23 +0200

    upload default project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SquirrelChat::Application.config.secret_token = '06f97be504e203ec1e1fd57ef08167ed0ac5a76347077ee76cb0f273fc4ae302d785a357a1f866a8583701c4ff7f805bd1ef41097a159eac4cdb4af54a3a3f09'
