e5d076b548d69e09ea462e4972ec46f786fb9355
Tom Lauwyck <tom.lauwyck@gmail.com>
2012-05-08 22:34:56 +0200

    Houston we have a working chatbox

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -6,6 +6,19 @@ class ChatController < ApplicationController
     ##TODO: This is where we could read older chat messages from file
     @message_list = session[:tmp_message_list]
     @message_list ||= "making the message not nil the first time"
+    ap "right before if statement"
+    @lines
+    if File.exist?("log/chat.log")
+      ap "File exists"
+      ##logfile = File.new("log/chat.log")
+     f = File.open("log/chat.log")
+      @lines = f.readlines
+      
+    else
+      ap "File does not exist"
+      chatlog = File.new("log/chat.log", "w")
+      chatlog.close
+    end
   end
   
   def create
@@ -16,6 +29,10 @@ class ChatController < ApplicationController
     
     
     ##TODO: This is where we could write the message to a file
+    open("log/chat.log", 'a') do |f|
+    f << @message + "\n"
+    end
+    
     
     session[:tmp_message_list] = @message_list
     redirect_to chat_path
