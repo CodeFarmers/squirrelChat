ec5a5308329ce58b274d8acef3b07f4711edf143
Tom Lauwyck <tom.lauwyck@gmail.com>
2012-05-08 18:47:35 +0200

    added session variable for connecting chatController actions

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,12 +3,15 @@ class ChatController < ApplicationController
     puts "In chatController NEW"
     @nickname = session[:tmp_nickname]
     ##TODO: This is where we could read older chat messages from file
-    @message = "This is a hard-coded test message"
+    @message = session[:tmp_message]
+    @message ||= "making the message not nil the first time"
   end
   
   def create
     @message = params[:message]
+    
     ##TODO: This is where we could write the message to a file
+    session[:tmp_message] = "a testmessage in the create action"  ##params[:nickname]
     
     redirect_to chat_path
   end
