220c6db5f565ef7dce9669f2c5f0da605646b9bd
Tom Lauwyck <tom.lauwyck@gmail.com>
2012-05-28 21:29:58 +0200

    Added 'Anonymous' for users without a nickname

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     
+    unless params[:nickname]
+      @user.nickname = "Anonymous"
+    end
+    
     if @user.save
       #handle successful save
-      
       ap "In UsersController#create " + @user.nickname
-      
       store_user_for_session(@user)
       redirect_to new_post_path
     else
