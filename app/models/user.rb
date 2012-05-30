03746c7ba48b73285b3de9d52fc6090d64d5567c
Tom Lauwyck <tom.lauwyck@gmail.com>
2012-05-30 22:16:12 +0200

    Removed old spec files and started with first model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :nickname
   has_many :posts, dependent: :destroy
+  validates :nickname, presence: true, length: { maximum: 50 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before { @user = User.new(nickname: "Tyrion") }
+  
+  subject { @user }
+
+  it { should respond_to(:nickname) }
+  it { should be_valid }
+  
+  describe "when nickname is not present" do
+    before { @user.nickname = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when name is too long" do
+    before { @user.nickname = "a" * 51 }
+    it { should_not be_valid }
+  end
 end
